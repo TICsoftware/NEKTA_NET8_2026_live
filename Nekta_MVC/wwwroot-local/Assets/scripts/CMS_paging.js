2d5e4aac4312c141bfbce6bc878d171a2641a169
tic.access@ticworks.com <padma.pritha@ticworks.com>
2026-07-21 15:47:05 +0530

    changes

--- Nekta_MVC/wwwroot-local/Assets/scripts/CMS_paging.js
+++
@@ -1,8 +0,0 @@
-$(document).on('click', '.paginationgrid a', function (e) {
-        e.preventDefault(); 
-        $(this).closest("form")
-            .find('[name="page_no"]')
-            .val($(this).data("page_no"));
-
-        $(this).closest('form').submit(); 
-});
--- Nekta_MVC/wwwroot-local/Assets/scripts/contextfield_index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.addEventListener("click", function (e) {
-        var link = e.target.closest(".js-confirm-deactivate");
-        if (link && !confirm("Deactivate this field?")) {
-            e.preventDefault();
-        }
-    });
-});
